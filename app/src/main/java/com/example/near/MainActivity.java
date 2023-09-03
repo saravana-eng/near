18750f43d72c746438ebfc3e6bba40fe0b6f1b4d
saravana <saravana.rae@gmail.com>
2023-09-04 00:46:53 +0530

    Redesigned mainactivity

--- app/src/main/java/com/example/near/MainActivity.java
+++ app/src/main/java/com/example/near/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
 
         Button next_button = (Button) findViewById(R.id.next_button);
